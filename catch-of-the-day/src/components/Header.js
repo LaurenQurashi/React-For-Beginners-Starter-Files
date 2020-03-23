782a9303e6e998422af261827c4207ee69d99b61
Lauren <laurenqurashi@me.com>
2020-03-23 12:29:41 +0000

    Stateless functional components - Made the header component a stateless component. - This means it is just a function that renders html. - Added comments for explanantion.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
+import { database } from "firebase";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
+const Header = props => (
+    <header className="top">
                 <h1>
                     Catch 
                     <span className="ofThe">
@@ -13,12 +12,13 @@ class Header extends React.Component {
                     Day
                 </h1>
                 <h3 className="tagline">
-                    <span>{this.props.tagline}</span>
-                    {/* This is how you display a prop in react */}
+                    <span>{props.tagline}</span>
                 </h3> 
-            </header>     
-        )
-    }
-}
+            </header> 
 
+);
 export default Header; 
+
+// Here you can have a stateless Component. If your component just renders HTML and gets fed in data, there
+// is no need for it to be a full blown component. We have made header into a stateless component by making it a 
+// function with an arrow statement and an implicit return.
