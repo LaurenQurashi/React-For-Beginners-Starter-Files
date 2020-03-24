5a9367e5eeca46f3b22c41f329deef324628fb1d
Lauren <laurenqurashi@me.com>
2020-03-24 15:47:48 +0000

    Added Event Handling to the StorePicker. - Added code to prevent the default behaviour of submitting happening. - Added a variable that grabs the input from the form. - Used history.push from the router to append the new URL. - Learned how to assign functions to props in order to access 'this'.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+    goToStore = event => {
+        // This stops the default behaviour of submitting the form and refreshing the page.
+        event.preventDefault();
+        // This grabs the value that have been entered into the form and puts it in a variable. 
+       const storeName = this.myInput.current.value;
+        // As store picker is a child of router, you can access history and push from the router. This adds the input value to the end of the url.
+        this.props.history.push(`/store/${storeName}`);
+    };
+
+    // You need to write a non built in method like a prop with a function assigned to it in order to reference 'this'. 
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit" >Visit Store</button>
             </form>
 
