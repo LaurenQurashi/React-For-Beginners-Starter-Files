ad68cd738ef27f55a6fab063e12228117c889e39
Lauren <laurenqurashi@me.com>
2020-03-24 22:23:55 +0000

    Video 15 - Displaying State - Added a fish component which shows individual fish on the menu - Imported a helper into that component. - In App we loop through the fishes in a list.

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+
+class Fish extends React.Component {
+    render() {
+        const { image, name, price, desc, status} = this.props.details;
+        // this is using Destructuring to assign the values to these variable names. 
+
+        return (
+            <li className="menu-fish">
+                <img src={image} alt={name}/>
+                <h3 className="fish-name">{name}</h3>
+                <span className="price">{formatPrice(price)}</span>
+                <p>{desc}</p>
+                <button>Add to Cart</button>
+            </li>    
+        )
+    }
+}
+
+export default Fish; 
